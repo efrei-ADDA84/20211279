d162b2a151c940ed2b7a22e8daf69828a4799455
ZacharyAk30 <96988490+ZacharyAk30@users.noreply.github.com>
2023-04-28 09:46:04 +0200

    wrapper_to_api_flask

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from flask import Flask, jsonify
 import config
 import requests
+
+app = Flask(__name__)
+
+
+@app.route("/weather")
+def get_current_weather():
+    lat = config.LAT
+    lon = config.LONG
+    api_key = config.API_KEY
+    weather_data = get_weather(api_key, lat, lon)
+    return jsonify(weather_data)
 
 
 def get_weather(api_key, lat, lon):
@@ -11,13 +23,18 @@ def get_weather(api_key, lat, lon):
         temp = data["main"]["temp"]
         feels_like = data["main"]["feels_like"]
         humidity = data["main"]["humidity"]
-        return f"Current weather: {description}, temperature: {temp}, feels like: {feels_like}, humidity: {humidity}"
+        weather_data = {
+            "description": description,
+            "temp": temp,
+            "feels_like": feels_like,
+            "humidity": humidity,
+        }
+        return weather_data
     else:
-        return (
-            f"Error retrieving weather data , {response.status_code}-{response.reason}"
-        )
+        return {
+            "error": f"Error retrieving weather data , {response.status_code}-{response.reason}"
+        }
 
 
 if __name__ == "__main__":
-    print("your params are :", config.API_KEY, config.LAT, config.LONG)
-    print(get_weather(config.API_KEY, config.LAT, config.LONG))
+    app.run(debug=True)
